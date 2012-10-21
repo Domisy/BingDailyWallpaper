a0c6ed36f8fe1dbc69b4102d4dca1100afe780c5
Theodore Mavrakis <tjmavrakis@live.com>
2012-10-20 23:16:59 -0500

    Rewrote init process.
    Fixed double initialization bug. Simplified init code. Updated icons and
    name.

--- src/com/rim/samples/device/helloworlddemo/AboutScreen.java
+++ src/com/rim/samples/device/helloworlddemo/AboutScreen.java
@@ -17,16 +17,6 @@ public class AboutScreen extends MainScreen {
 	public AboutScreen() {
 		super(Manager.VERTICAL_SCROLL | Manager.VERTICAL_SCROLLBAR);
 		
-		/*Font fonts[] = new Font[3];
-		int[] offset = new int[4];
-		byte[] attribute = new byte[3];
-		fonts[0] = Font.getDefault();
-	    fonts[1] = Font.getDefault().derive(Font.BOLD);
-	    fonts[2] = Font.getDefault().derive(Font.BOLD | Font.ITALIC);
-	    offset[0] = 0;
-	    attribute[0] = 2;
-	    //offset[3] = 
-*/		
 		VerticalFieldManager vfm = new VerticalFieldManager(USE_ALL_WIDTH | FIELD_HCENTER){
 			protected void sublayout(int maxWidth, int maxHeight) { 
 				 
@@ -36,10 +26,11 @@ public class AboutScreen extends MainScreen {
             } 
 
 	    };
+	    //For 7.0+ use TEXT_ALIGN_HCENTER, and 6.0 use TEXT_JUSTIFY_HCENTER
 	    vfm.add(new RichTextField("All images provided by Bing.com.", Field.NON_FOCUSABLE | RichTextField.TEXT_ALIGN_HCENTER));
 		vfm.add(new RichTextField("This application is in no way affiliated with Bing or Microsoft corporations.", Field.NON_FOCUSABLE | RichTextField.TEXT_ALIGN_HCENTER));
 		vfm.add(new SpacerField(0, 25));
-		vfm.add(new RichTextField("Bing Daily Wallpaper was brought to you by - Domisy Dev", Field.NON_FOCUSABLE | RichTextField.TEXT_ALIGN_HCENTER));
+		vfm.add(new RichTextField("Daily Wallpaper - with Bing was brought to you by - Domisy Dev", Field.NON_FOCUSABLE | RichTextField.TEXT_ALIGN_HCENTER));
 		vfm.add(new SpacerField(0, 25));
 		vfm.add(new RichTextField("Lead Developer - Theodore Mavrakis", Field.NON_FOCUSABLE | RichTextField.TEXT_ALIGN_HCENTER));
 		vfm.add(new SpacerField(0, 25));
